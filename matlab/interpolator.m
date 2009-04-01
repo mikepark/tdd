509779494d95276cd3d88bce40a6dcae0660f171
Mike Park <MikePark.rb@GMail.com>
2009-04-01 12:39:44 -0400

    simpler

--- matlab/interpolator.m
+++ matlab/interpolator.m
@@ -4,11 +4,12 @@ interpolant = NaN;
 
 if ( 3 == nargin )
   s2 = (lookup_value - x_table(1)) / ...
-       (x_table(2)   - x_table(1))
+       (x_table(2)   - x_table(1));
   s1 = 1-s2;
-  interpolant = s2 * y_table(2) + s1 * y_table(1)
+  interpolant = s2 * y_table(2) + s1 * y_table(1);
 end
 
 %! assert_nan(interpolator(),'does not return error with < 3 arguments')
-%! assert_equal(3,interpolator([0,1],[2,4],0.5),'does not return mdipouint value')
+%! assert_equal(3,interpolator([0,1],[2,4],0.5),'does not return midpoint value')
+%! assert_equal(2,interpolator([0,1],[2,4],0.0),'does not return endpoint value')
 
