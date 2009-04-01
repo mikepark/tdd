07b8fd40521c90a2a0d9978925d6fb2737bb0b7f
Mike Park <MikePark.rb@GMail.com>
2009-04-01 12:37:37 -0400

    simpler

--- matlab/interpolator.m
+++ matlab/interpolator.m
@@ -3,26 +3,12 @@ function interpolant = interpolator(x_table, y_table, lookup_value)
 interpolant = NaN;
 
 if ( 3 == nargin )
-  [closest_value, closest_index] = min(abs(lookup_value-x_table))
-  if ( lookup_value > x_table(closest_index) )
-    if ( closest_index < length(x_table) )
-      s1 = (lookup_value             - x_table(closest_index)) / ...
-           (x_table(closest_index+1) - x_table(closest_index))
-      s0 = 1-s1;
-      interpolant = s1 * y_table(closest_index+1) + s0 * y_table(closest_index)
-    end
-  else
-    if ( closest_index > 1 )
-      s1 = (lookup_value           - x_table(closest_index-1)) / ...
-           (x_table(closest_index) - x_table(closest_index-1))
-      s0 = 1-s1;
-      interpolant = s1 * y_table(closest_index) + s0 * y_table(closest_index-1)
-    end
-  end
+  s2 = (lookup_value - x_table(1)) / ...
+       (x_table(2)   - x_table(1))
+  s1 = 1-s2;
+  interpolant = s2 * y_table(2) + s1 * y_table(1)
 end
 
 %! assert_nan(interpolator(),'does not return error with < 3 arguments')
-%! assert_nan(interpolator([0,1],[2,4],5),'does not return error with high lookup-value')
-%! assert_nan(interpolator([0,1],[2,4],-1),'does not return error with low lookup-value')
-%! assert_equal(3,interpolator([0,1],[2,4],0.5),'does not return error with outside lookup-value')
+%! assert_equal(3,interpolator([0,1],[2,4],0.5),'does not return mdipouint value')
 
